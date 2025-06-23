5e48392aecf18e3b78ebad0667345fefc2c714e8
Tiisetsopudinyane <pudinyanesamueltiisetso@gmail.com>
2025-06-23 17:49:07 +0200

    n

--- app.py
+++ app.py
@@ -3409,13 +3409,11 @@ def supervisor_dashboard():
     supervisor_formB = db_session.query(FormB).join(User, FormB.user_id == User.user_id).join(FormARequirements, FormARequirements.user_id == FormB.user_id).filter(User.supervisor_id == supervisor_id).all()
 
     supervisor_formC = db_session.query(FormC).join(User, FormC.user_id == User.user_id).join(FormARequirements, FormARequirements.user_id == FormC.user_id).filter(User.supervisor_id == supervisor_id).all()
-
+    for form in supervisor_formC:
+        print(form.FormC)
     #supervisor_formA_req=db_session.query(model).filter(FormARequirements.user_id == FormA.user_id).all()
-    
-    
 
     return render_template("supervisor-dashboard.html",supervisor_role=supervisor_role,formA=formA,formB=formB,formC=formC,supervisor_formA=supervisor_formA,supervisor_formB=supervisor_formB,supervisor_formC=supervisor_formC)
-
 @app.route('/dean_dashboard', methods=['GET','POST'])
 def dean_dashboard():
     role="STUDENT"
