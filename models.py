3343e43f6e3f775d855df485f03f986f9fed105f
HopeLotriet <hopelotriet@gmail.com>
2025-08-13 17:52:27 +0300

    "Changes"

--- models.py
+++ models.py
@@ -20,17 +20,17 @@ import json
 # db_path = os.path.join(os.path.dirname(__file__), "ethics.db")
 # sqlite_string = f"sqlite+pysqlite:///{db_path}"
 
-connection_string = (
-    "mssql+pyodbc://@APB-JBS02-113L\\SQLEXPRESS/ethics?"
-    "driver=ODBC+Driver+17+for+SQL+Server&"
-    "trusted_connection=yes"
-)
-
 # connection_string = (
-#     "mssql+pyodbc://@MOOSE\\SQLEXPRESS/ethics?"
+#     "mssql+pyodbc://@APB-JBS02-113L\\SQLEXPRESS/ethics?"
 #     "driver=ODBC+Driver+17+for+SQL+Server&"
 #     "trusted_connection=yes"
 # )
+
+connection_string = (
+    "mssql+pyodbc://@MOOSE\\SQLEXPRESS/ethics?"
+    "driver=ODBC+Driver+17+for+SQL+Server&"
+    "trusted_connection=yes"
+)
 
 engine = create_engine(connection_string, echo=True)
 
